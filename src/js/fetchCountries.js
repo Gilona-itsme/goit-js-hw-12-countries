c162a42893afd5feaeed928948b2f272143dbb32
Ilona <ilona.gil@outlook.com>
2020-11-13 10:41:56 +0200

    Обновляет скрипт 13/11

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,47 +2,35 @@ import '../styles.scss';
 import countryCardTpl from '../templates/countries-card.hbs';
 import countryListTpl from '../templates/countries-list.hbs';
 import API from './api-service';
-import getRefs from "./get-refs";
-import "@pnotify/core/dist/PNotify.css";
+import getRefs from './get-refs';
+import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
-
 
 const refs = getRefs();
 const debounce = require('lodash.debounce');
-const  {error}  = require('@pnotify/core');
-//renderCountryCard();
+const { error } = require('@pnotify/core');
 
-refs.searchInput.addEventListener('input', debounce(onSearch, 1000));
+refs.searchInput.addEventListener('input', debounce(onSearch, 500));
 
-
- function onSearch(e) {
+function onSearch(e) {
   e.preventDefault();
   const inputValue = e.target.value;
-    
-console.log(inputValue);
+
+  console.log(inputValue);
   API.fetchCountryByName(inputValue).then(data => {
-   if (data) {
-     if (data.length > 10) {
-       error({
-         text: 'Too many mathces found. Please enter a more specific query!',
-         type: "info"
-       });
-     } else if (data.length < 2) {
-       renderCountryCard(data[0]);
-     } else if (data.length >= 2 && data.length <= 10) {
-       renderCountryList(data);
-     }
-     
+    if (data.length > 10) {
+      error({
+        text: 'Too many mathces found. Please enter a more specific query!',
+        type: 'info',
+      });
+    } else if (data.length < 2) {
+      renderCountryCard(data[0]);
+    } else if (data.length >= 2 && data.length <= 10) {
+      renderCountryList(data);
     }
-    
- });
-//     .catch(onFetchError)
-//     .finally(() => inputValue.reset);
- }
+  });
+}
 
-
-
- 
 function renderCountryCard(country) {
   const markUp = countryCardTpl(country);
   refs.cardContainer.innerHTML = markUp;
@@ -53,13 +41,4 @@ function renderCountryList(countries) {
   refs.cardContainer.innerHTML = listUp;
 }
 
-function clear() {
-  refs.cardContainer.innerHTML = "";
-  
-}
-// function onFetchError(error) {
-//   alert('404');
-// }
-
 export default { onSearch };
-
